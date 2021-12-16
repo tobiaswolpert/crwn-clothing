8d7100f884b803f326b62e11b6b9a60374d69ece
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@zf.com>
2021-12-16 09:04:19 -0500

    broken homepage into smaller components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import './directory.styles.scss';
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            sections: [{
+              title: 'hats',
+              imageUrl: '/images/hats.png',
+              id: 1,
+              linkUrl: 'shop/hats'
+            },
+            {
+              title: 'jackets',
+              imageUrl: '/images/jackets.png',
+              id: 2,
+              linkUrl: 'shop/jackets'
+            },
+            {
+              title: 'sneakers',
+              imageUrl: '/images/sneakers.png',
+              id: 3,
+              linkUrl: 'shop/sneakers'
+            },
+            {
+              title: 'womens',
+              imageUrl: '/images/womens.png',
+              id: 4,
+              linkUrl: 'shop/womens',
+              size: 'large'
+            },
+            {
+              title: 'mens',
+              imageUrl: '/images/men.png',
+              id: 5,
+              linkUrl: 'shop/mens',
+              size: 'large'
+            }
+          ]};
+    }    
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {this.state.sections.map(section => <MenuItem key={section.id} title={section.title} url={section.imageUrl} size={section.size}/>)}
+            </div>
+        )
+    }    
+}
+
+export default Directory;
