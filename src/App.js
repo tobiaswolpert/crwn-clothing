fa217f9eb9331b26e4c601bf7b865c3ca7ba0b5c
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@zf.com>
2021-12-17 10:32:35 -0500

    updating the app with shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => {
-  return (<div>
-    <h1>HatsPage</h1>
-  </div>
-  )};
+import ShopPage from './components/shop/shop.component';
 
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/hats' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
     </Switch>
   );
 }
