2621d2e7d58bb3c8f4981bd2532abb91c463e938
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@zf.com>
2022-01-03 12:16:20 -0500

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import './collection-item.styles.scss';
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions"; 
+import { connect } from "react-redux";
+import cartReducer from "../../redux/cart/cart.reducer";
 
-const CollectionItem = ({id, name, price, imageUrl}) => {
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;    
+    
     return (
         <div className="collection-item">
             <div className="image" style={{
@@ -11,10 +17,18 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-                
+            <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>             
             
         </div>
     )
 }
 
-export default CollectionItem;
+const mapStateToProps = ({ cart }) => ({
+    cartItems: cartReducer.cartItems
+})
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,13 +1,17 @@
 import { CartActionType } from "./cart.types";
+import { addItemToCart } from "./cart.utils"; 
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state=INITIAL_STATE, action) => {
     switch(action.type) {
         case CartActionType.TOGGLE_CART_HIDDEN:
             return Object.assign({},INITIAL_STATE, {hidden: !state.hidden})
+        case CartActionType.ADD_ITEM:
+            return {...state, cartItems: addItemToCart(state.cartItems, action.payload)}
         default:
             return state;
         }
