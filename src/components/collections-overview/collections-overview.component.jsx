caad2de9ee52bbe192f0e6a368f51708509a1c1d
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-01-17 14:54:54 -0500

    moved directory and shop data in its own reducer

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './collections-overview.styles.scss';
 import { connect } from 'react-redux';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
 import PreviewCollection from '../preview-collection/preview-collection.component';
 
 const CollectionsOverview = ({ collections }) => {
     return (
         <div className='collections-overview'>
-              {collections.map(({id, ...otherCollectionProps}) => 
-                        <PreviewCollection key={id} {...otherCollectionProps}/>)}
+              {collections.map(({id, ...otherCollectionProps}) => <PreviewCollection key={id} {...otherCollectionProps}/>)}
         </div>
     )
 } 
 
 const mapStateToProps = state =>({
-    collections: selectCollections(state)
+    collections: selectCollectionForPreview(state)
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,6 +5,7 @@ import CollectionPage from "../collection/collection.component";
 
 
 const ShopPage = ({ match }) => {
+        
         return (
             <div className="shop-page">
                 <Route exact path={`${match.path}`} component={CollectionsOverview}/>
